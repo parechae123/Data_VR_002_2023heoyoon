89001007d4be6b33a9a13e2b3eb00d4e2a352313
parechae123 <parechae@gmail.com>
2023-04-24 20:29:54 +0900

    디자인패턴 싱글톤,추상클래스

---
+++ DataGitClass/Assets/SelfStudy/DesignPatternStudy/AbsTest/PlayerLists.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLists : MonoBehaviour
+{
+
+}
+public class son : Players
+{
+    public override void PlayerName()
+    {
+        Debug.Log("HeungMin");
+    }
+}
+public class han : Players
+{
+    public override void PlayerName()
+    {
+        Debug.Log("BanDu");
+    }
+}
+public class yoon : Players
+{
+    public override void PlayerName()
+    {
+        Debug.Log("SukYoung");
+    }
+}
+public class gimotti : Players
+{
+    public override void PlayerName()
+    {
+        Debug.Log("messi");
+    }
+}
+
---
+++ DataGitClass/Assets/SelfStudy/DesignPatternStudy/AbsTest/PlayerOutput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOutput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        han hou = new han();
+        hou.PlayerName();
+        son min = new son();
+        min.PlayerName();
+        yoon yoony = new yoon();
+        yoony.PlayerName();
+        gimotti mess = new gimotti();
+        mess.PlayerName();
+    }
+
+}
